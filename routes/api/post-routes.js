34e4d8c2017a3f94cabbb6fd356f63da5a101185
Felicity Brammer <felicitybrammer@gmail.com>
2022-03-10 16:24:19 -0700

    post-routes add voting relationships

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Vote } = require('../../models');
+const sequelize = require('../../config/connection');
 
 // GET all posts
 router.get('/', (req, res) => {
@@ -62,6 +63,38 @@ router.post('/', (req, res) => {
         });
 });
 
+// PUT /api/posts/upvote
+router.put('/upvote', (req, res) => {
+    Vote.create({
+        user_id: req.body.user_id,
+        post_id: req.body.post_id
+    })
+        .then(() => {
+            //then find the post we just voted on
+            return Post.findOne({
+                where: {
+                    id: req.body.post_id
+                },
+                attributes: [
+                    'id',
+                    'post_url',
+                    'title',
+                    'created_at',
+                    [
+                        sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+                        'vote_count'
+                    ]
+                ]
+            })
+                .then(dbPostData => res.json(dbPostData))
+                .catch(err => {
+                    console.log(err);
+                    res.status(400).json(err);
+                });
+        });
+});
+
+// UPDATE a post
 router.put('/:id', (req, res) => {
     Post.update(
       {
@@ -86,7 +119,8 @@ router.put('/:id', (req, res) => {
       });
   });
 
-  router.delete('/:id', (req, res) => {
+// DELETE post  
+router.delete('/:id', (req, res) => {
     Post.destroy({
       where: {
         id: req.params.id
