a2cbc8b22fc6f99748631f87f540ad308b62daea
Felicity Brammer <felicitybrammer@gmail.com>
2022-03-11 16:50:45 -0700

    conditional rendering based on login state

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -85,13 +85,14 @@ router.post('/', (req, res) => {
 
 // PUT /api/posts/upvote
 router.put('/upvote', (req, res) => {
-  if (req.session) {  
-  
-    Post.upvote({ ...req.body, user_id: req.session.user_id },{ Vote, Comment, User })
+  // make sure the session exists first
+  if (req.session) {
+    // pass session id along with all destructured properties on req.body
+    Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
       .then(updatedVoteData => res.json(updatedVoteData))
       .catch(err => {
         console.log(err);
-        res.status(400).json(err);
+        res.status(500).json(err);
       });
   }
 });
