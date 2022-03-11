1ec0507dc85e0bb39ca1973dfc4e15e9ca2f506b
Felicity Brammer <felicitybrammer@gmail.com>
2022-03-11 11:57:54 -0700

    homepage templating done for posts

--- server.js
+++ server.js
@@ -4,14 +4,13 @@ const sequelize = require('./config/connection');
 
 const path = require('path');
 
+const app = express();
+const PORT = process.env.PORT || 3001;
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-
-const app = express();
-const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
